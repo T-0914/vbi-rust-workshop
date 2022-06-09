ee51aaa8972772a759f0e7250f358dd8619ac231
shyoski <ngonhuthanhtrung1409@gmail.com>
2022-06-10 06:42:15 +0700

    :white_check_mark: Adding unit test!

--- 09_06_2022/sublist/src/main.rs
+++ 09_06_2022/sublist/src/main.rs
@@ -34,3 +34,35 @@ fn is_sublist(array: &[i32], other_array: &[i32]) -> bool {
 
     return false;
 }
+
+#[test]
+fn test_empty_array_is_sublist() {
+    let first_array: [i32; 0] = [];
+    let second_array: [i32; 3] = [6, 8, 10];
+
+    assert_eq!(is_sublist(&first_array, &second_array), true)
+}
+
+#[test]
+fn test_first_array_is_included_in_second_array() {
+    let first_array: [i32; 2] = [6, 8];
+    let second_array: [i32; 3] = [6, 8, 10];
+
+    assert_eq!(is_sublist(&first_array, &second_array), true)
+}
+
+#[test]
+fn test_first_array_is_not_included_in_second_array() {
+    let first_array: [i32; 2] = [6, 9];
+    let second_array: [i32; 3] = [6, 8, 10];
+
+    assert_eq!(is_sublist(&first_array, &second_array), false)
+}
+
+#[test]
+fn test_first_array_is_not_included_in_second_array_2() {
+    let first_array: [i32; 4] = [6, 8, 10, 11];
+    let second_array: [i32; 3] = [6, 8, 10];
+
+    assert_eq!(is_sublist(&first_array, &second_array), false)
+}
